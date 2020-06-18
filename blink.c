c7b9dc8d79483d14951a2a52da8ff12748c21f9a
jsferrarelli <ferrarellij@wit.edu>
2020-06-18 17:07:02 -0400

    Rewrite to blink using interrupts rather than delays

--- blink.c
+++ blink.c
@@ -1,17 +1,36 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdbool.h>
+#include <avr/interrupt.h>
 
-#define BLINK_MS 150
+#define PRESCALER 10 		// 10 bits...1024
+#define CLOCK_RATE 15998976
+
+// approximately number of ticks in a second
+#define COUNTER_VALUE (CLOCK_RATE >> PRESCALER)
+
+ISR(TIMER1_COMPA_vect) {
+	PORTB ^= _BV(PORTB5);
+}
 
 int main() {
 	// pin mode for port B, set to output mode
 	DDRB |= _BV(PORTB5); 
 
+	// output compare register 1, set to number of clock ticks for interrupt
+	OCR1A = COUNTER_VALUE;
+
+	// config the timer, (Timer Counter Config Register)
+	TCCR1A = 0x00;
+	TCCR1B |= (1 << WGM12); // clear timer on compare
+	TCCR1B |= (1 << CS10) | (1 << CS12);
+
+	// send the interrupt
+	TIMSK1 |= (1 << OCIE1A);
+	sei();	// enable interupts
+
 	while(true) {
-		// toggle LED on/off (as opposed to turn on, then turn off)
-		PORTB ^= _BV(PORTB5);
-		_delay_ms(BLINK_MS);
+	
 	}
 
 
